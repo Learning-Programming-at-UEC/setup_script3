13ca788f12e60ecca95cd060d783ea6ccbe5ef3b
YaGiNA <yutachihaya@gmail.com>
2017-04-23 09:00:26 +0900

    Split to 2 .sh

---
+++ pg_setup3-next.sh
@@ -0,0 +1,8 @@
+# 先にpg_setup.sh を実行してください
+
+pyenv install anaconda3-4.3.1
+pyenv global anaconda3-4.3.1
+echo 'export PATH="$PYENV_ROOT/versions/anaconda3-4.3.1/bin/:$PATH"' >> ~/.bashrc
+conda update -y conda
+conda update -y --all
+conda create -n uec python=3.6 anaconda
--- pg_setup3.sh
+++ pg_setup3.sh
@@ -16,9 +16,3 @@ echo 'export PYENV_ROOT="${HOME}/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 exec $SHELL -l
-pyenv install anaconda3-4.3.1
-pyenv global anaconda3-4.3.1
-echo 'export PATH="$PYENV_ROOT/versions/anaconda3-4.3.1/bin/:$PATH"' >> ~/.bashrc
-conda update -y conda
-conda update -y --all
-conda create -n uec python=3.6 anaconda
