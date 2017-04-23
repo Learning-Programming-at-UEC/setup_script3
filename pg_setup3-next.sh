b507a36900388fc8192ebda09ab996c106cd183a
YaGiNA <yutachihaya@gmail.com>
2017-04-23 09:24:30 +0900

    :bug:Debug

--- pg_setup3-next.sh
+++ pg_setup3-next.sh
@@ -2,7 +2,7 @@
 
 pyenv install anaconda3-4.3.1
 pyenv global anaconda3-4.3.1
-echo 'export PATH="$PYENV_ROOT/versions/anaconda3-4.3.1/bin/:$PATH"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/versions/anaconda3-4.3.1/bin/:$PATH"' >> ~/.bash_profile
 conda update -y conda
 conda update -y --all
 conda create -n uec python=3.6 anaconda
